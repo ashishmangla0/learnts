ac34cc788c369615af17dc1bd731b5dee5fae75c
Ashish Mangla <ashmangl@publicisgroupe.net>
2022-06-02 10:52:11 +0530

    a

--- components/heading/heading.tsx
+++ components/heading/heading.tsx
@@ -2,11 +2,12 @@
   type propsHeading = {
     level: number,
     title?: string,
-    className ?: string
+    className ?: string,
+    H: JSX.IntrinsicElements
   }
   const Heading: React.FC<propsHeading> = (props) => {
       const {title,level} = props;
-    const H  = `h${level}` as keyof JSX.IntrinsicElements;
+    const H  = `h${level}`
     return (
         <>
             <H>
